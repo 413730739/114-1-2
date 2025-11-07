49cfb6383a05e7e40c7d049f848ded55fe3fdeab
413730739 <413730739@o365.tku.edu.tw>
2025-11-07 23:54:36 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -68,7 +68,7 @@ function getNonOverlappingPosition(letterSize) {
     newY = random(margin * 3, height - margin);
     for (let existing of letters) {
       // 將距離門檻放寬，例如 2.5 倍字母大小
-      if (dist(newX, newY, existing.x, existing.y) < letterSize * 2.5) {
+      if (dist(newX, newY, existing.x, existing.y) < letterSize * 3.5) {
         overlapping = true;
         break;
       }
@@ -142,8 +142,8 @@ function mouseReleased() {
 
           // 排列成上方配對區
           const pairsPerRow = 10;
-          const pairGroupWidth = 230;// 每對字母組寬度
-          const letterInPairOffset = 95;// 同一對字母間距
+          const pairGroupWidth = 180;// 每對字母組寬度
+          const letterInPairOffset = 60;// 同一對字母間距
           const rowHeight = 110;// 每行高度
           const startX = 70;// 起始X座標
           const startY = 90;
